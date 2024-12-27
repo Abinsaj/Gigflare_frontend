072650505b10b5f893a24dee8eacf7d217c9eb07
Abinsaj <148989575+Abinsaj@users.noreply.github.com>
2024-12-27 19:49:21 +0530

    adminurlchange

--- src/Redux/actions/adminAction.ts
+++ src/Redux/actions/adminAction.ts
@@ -9,7 +9,7 @@ export const verifyAdmin = createAsyncThunk(
     'admin/authlogin',
     async({email, password}:{email: string, password: string},thunkAPI)=>{
         try {
-            const response = await axios.post(`${url}/verifyAdmin`,{email, password},{withCredentials:true})
+            const response = await axios.post(`https://www.gigflare.online/admin/verifyAdmin`,{email, password},{withCredentials:true})
             return response.data
         } catch (error: any) {
             return thunkAPI.rejectWithValue(error.response.data || error.message)
